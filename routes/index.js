c3b034f12a00d95d33275d21328a380de169fef6
pkumza <pkumza@163.com>
2016-09-13 10:13:46 +0800

    提供访问接口

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var log4js = require('log4js');
 var fs = require('fs');
 
-if (!fs.existsSync('logs')) {
-  fs.mkdirSync('logs');
+if (!fs.existsSync('public/logs')) {
+  fs.mkdirSync('public/logs');
 }
 
 log4js.configure({
@@ -12,7 +12,7 @@ log4js.configure({
     {type: 'console'},
     {
       type: 'file',
-      filename: 'logs/AppFeather.log',
+      filename: 'public/logs/AppFeather.log',
       maxLogSize: 102400,
       backups:10000,
       category: "feather"
@@ -27,6 +27,17 @@ router.get('/', function(req, res, next) {
   // console.log(req.query.data);
   feather_log.trace(req.query.data);
   res.render('index', { title: 'AppFeather 应用の翼' });
+});
+
+router.get('/get_data', function(req, res, next){
+  var usfs = fs.readdirSync("public/logs");
+  var links = new Array();
+  usfs.forEach(function(file){
+    file = "/logs/" + file;
+    links.push(file);
+  });
+  console.log(links);
+  res.render('get_data', { title: 'Data Recorded.', data: links});
 });
 
 router.get('/')
